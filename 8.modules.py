049184b1825898a147702ac5086f08cb408c9b9b
honorezemagho <honorezemagho@gmail.com>
2020-08-17 06:53:06 +0100

    read csv file using pandas

--- 8.modules.py
+++ 8.modules.py
@@ -1,11 +1,22 @@
 import  time
 import os
+import pandas
+
+# while True:
+#     if os.path.exists("files/vegetables.txt"):
+
+#         with open("files/vegetables.txt") as file:
+#             print(file.read())
+     
+#     else:
+#         print("File not found")
+#         time.sleep(10)
+
 
 while True:
-    if os.path.exists("files/vegetables.txt"):
-
-        with open("files/vegetables.txt") as file:
-            print(file.read())
+    if os.path.exists("files/temps_today.csv"):
+        data = pandas.read_csv("files/temps_today.csv")
+        print(data.mean())
      
     else:
         print("File not found")
